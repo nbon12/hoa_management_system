4d3c71fe790d53f8adaacf7ff8e6c4225540b697
Nicholas Bonilla <bossy-callow-7h@icloud.com>
2025-07-24 17:36:34 -0400

    add: ViolationType migration

---
+++ HOAManagementCompany/Migrations/20250724212415_InitialCreateAndViolationsAndViolationTypes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HOAManagementCompany.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateAndViolationsAndViolationTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ViolationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CovenantText = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ViolationTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Violations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ViolationTypeId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Violations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Violations_ViolationTypes_ViolationTypeId",
+                        column: x => x.ViolationTypeId,
+                        principalTable: "ViolationTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ViolationTypes",
+                columns: new[] { "Id", "CovenantText", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("3f843e9d-3e26-4696-84d6-f20f2dc20b1f"), "Owners must maintain exterior (placeholder)...", "POWERWASH" },
+                    { new Guid("b5a56c9b-a14f-4f9b-afc1-82d00663aa01"), "Owners must maintain lawn (placeholder)...", "GRASS" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Violations_ViolationTypeId",
+                table: "Violations",
+                column: "ViolationTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Violations");
+
+            migrationBuilder.DropTable(
+                name: "ViolationTypes");
+        }
+    }
+}
